59ba318bc9cfc200ea0c76310d940cf5def571a7
forwardtop <noproblemgod@hotmail.com>
2024-01-13 01:56:17 +0800

    action git

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,18 @@
 import styles from "@/styles/style";
+import { logo } from "@/public/assets";
 import { footerLinks, socialMedia } from "@/constants";
 import Image from "next/image";
 const Footer: React.FC = () => (
   <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
     <div className={`${styles.flexCenter} md:flex-row flex-col mb-8 w-full`}>
       <div className="flex-1 flex flex-col justify-start mr-10">
-        <h1 className="text-8xl text-white">QWIKIO</h1>
+        <Image
+          src={logo}
+          alt="hoobank"
+          className="w-[266px] h-[72px] object-contain"
+        />
         <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>
-          A new way to make the payment easy, reliable and secure.
+          A new way to make the payment easy, reliable amd secure.
         </p>
       </div>
       <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
@@ -38,7 +43,7 @@ const Footer: React.FC = () => (
     </div>
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
       <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-        2022 HooBank. All Rights Reserved.
+        2023 QWIKIO. All Rights Reserved.
       </p>
       <div className="flex flex-row md:mt-0 mt-6">
         {socialMedia.map((social, index) => (
